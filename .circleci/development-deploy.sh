bd5fa0dea2d112b1d50a28b9d9b0b2e029018c38
Andrés Fabián Ramos <andresfabi90@gmail.com>
2020-04-17 13:06:29 -0500

    Added ssh

--- .circleci/development-deploy.sh
+++ .circleci/development-deploy.sh
@@ -1,2 +1,3 @@
-ssh root@ec2-18-219-111-1.us-east-2.compute.amazonaws.com
+ssh -o StrictHostKeyChecking=no -l "root" "$ec2-18-219-111-1.us-east-2.compute.amazonaws.com"
+ssh -i ".circleci/instances.pem" root@ec2-18-219-111-1.us-east-2.compute.amazonaws.com
 yes
